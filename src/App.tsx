27668730f9447ee3235e0eba2f0df7b8ade9f916
RabinThapa1998 <imrabin1998@gmail.com>
2023-01-18 19:38:22 +0545

    feat: add router ,layout and details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Home } from '~/pages';
+import { Home, Details } from '~/pages';
+import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
+import { LayoutComponent } from '~/components';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <LayoutComponent>
+        <Home />
+      </LayoutComponent>
+    ),
+  },
+  {
+    path: '/:id',
+    element: (
+      <LayoutComponent>
+        <Details />
+      </LayoutComponent>
+    ),
+  },
+]);
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,56 @@
+import { Layout, Menu } from 'antd';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
+
+export const LayoutComponent = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { id } = useParams();
+  const secondPagePath = location.state?.secondPagePath || '/:id';
+  return (
+    <Layout className='site-layout'>
+      <Header
+        style={{
+          padding: '0 1.25rem',
+        }}
+      >
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['1']}
+          selectedKeys={id ? ['2'] : ['1']}
+          items={[
+            {
+              key: '1',
+              label: 'Home',
+              onClick: () =>
+                navigate('/', {
+                  state: {
+                    secondPagePath: secondPagePath,
+                  },
+                }),
+            },
+            {
+              key: '2',
+              label: 'Details',
+              onClick: () =>
+                navigate(secondPagePath, {
+                  state: {
+                    secondPagePath: secondPagePath,
+                  },
+                }),
+            },
+          ]}
+        />
+      </Header>
+      <Content className='site-layout-background h-full'>{children}</Content>
+      <Footer style={{ textAlign: 'center' }}>
+        GitHub Repository Search System ©2023 Created by{' '}
+        <a href='https://github.com/RabinThapa1998' target={'_blank'} rel='noreferrer'>
+          Rabin Thapa
+        </a>
+      </Footer>
+    </Layout>
+  );
+};
