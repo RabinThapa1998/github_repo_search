886c81c407fea2bbc833b8513ba627d6375228de
RabinThapa1998 <imrabin1998@gmail.com>
2023-01-19 21:49:09 +0545

    feat(server): add query validation

--- server/src/routes/v1/dashboard/search/get.ts
+++ server/src/routes/v1/dashboard/search/get.ts
@@ -1,7 +1,12 @@
 import Router from "express";
+import { query } from "express-validator";
 import { getSearchHandler } from "../../../../controller/v1/dashboard/search";
+import { validateRequest } from "../../../../common/middlewares";
 
 const router = Router();
+const validateFields = [
+  query("q").not().isEmpty().withMessage("query cannot be empty"),
+];
 
-router.get("/", getSearchHandler);
+router.get("/", validateFields, validateRequest, getSearchHandler);
 export { router as getSearchRouter };
